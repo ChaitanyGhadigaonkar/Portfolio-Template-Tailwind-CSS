15d7434382fd1228cff6627d4083ff628ea92b45
ChaitanyGhadigaonkar <chaitanyghadigaonkar28@gmail.com>
2023-01-17 11:23:19 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'backgroundImage': "url('/Images/bg.jpg')",
+  
+      }
+    },
+  },
+  plugins: [],
+}
